fad4790c62067a15e25efcc6ce35bd41561c9403
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-07-19 11:48:05 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as Styled from './styled';
+
+const HelloComponent = () => (
+  <Styled.Wrapper>
+    <h1>Hello</h1>
+  </Styled.Wrapper>
+);
+
+export default HelloComponent;
